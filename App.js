81cbedd79b2d8a5177266d062ec8c235c2e44f5f
Steve Kuznetsov <skuznets@redhat.com>
2022-12-22 08:30:52 -0700

    reproduce error
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+import { Card } from '@ant-design/react-native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Card/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
